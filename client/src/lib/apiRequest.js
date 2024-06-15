7896b76711c6f244978d86fcbff845161aacf4be
Gaven Dcosta <gavendcosta@gmail.com>
2024-06-15 08:50:09 +0530

    vercel api in ui

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://adoption-maria-rescue-camp-api.vercel.app/api",
     withCredentials: true,
 })
 
